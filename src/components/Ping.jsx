67b556c65e39b0e1ab3571f86da48d2487a8a503
Anisubhra Sarkar <anisubhrasarkar@gmail.com>
2022-05-03 21:51:00 +0530

    changes to loader

--- src/components/Ping.jsx
+++ src/components/Ping.jsx
@@ -105,6 +105,7 @@ const PingComponent = () => {
     const [isFinished, setIsFinished] = useState(false);
     const [recommendedRegion, setRecommendedRegion] = useState();
     const [pingCount, setPingCount] = useState();
+    const [isSorted, setIsSorted] = useState(false);
 
     const initPingRegions = () => {
         const regions = Object.entries(regionsMap);
@@ -154,6 +155,7 @@ const PingComponent = () => {
     const resetTest = () => {
         setIsReady(false);
         setPingCount(0);
+        setIsSorted(false);
     }
 
     const startPingTest = () => {
@@ -187,13 +189,26 @@ const PingComponent = () => {
         return minLatencyIndex;
     }
 
+    const sortRegions = () => {
+        const regions = [...pingResults];
+        const sortedRegions = regions.sort((a, b) => a.latency - b.latency);
+
+        // setPingResults(sortedRegions);
+        setIsSorted(true);
+    }
+
     useEffect(() => {
         if (isFinished) {
             console.log('Ping result =>', pingResults);
-            calculateRecommendRegion();
+            sortRegions();
             setTimeout(resetTest, 1000);
         }
     }, [isFinished])
+
+    useEffect(() => {
+        if (isSorted)
+            calculateRecommendRegion();
+    }, [isSorted])
 
     return (
         <div className="global_wrapper">
@@ -211,20 +226,20 @@ const PingComponent = () => {
                     <div className="d-flex">
                         <div className="element">
                             <h4 className="header-title">
-                                Media Region Test
+                                AWS Media Region Test
                             </h4>
                             <p>
-                                Latency ping test tool. Tests across various AWS Media Regions
-                                across the globe. Help user pick the most optimal media
-                                region.
+                                Test performance of various AWS media regions across the world.
+                                Lower latency is better for video calls.
+                                Best regions are sorted on the top with our recommended region.
                             </p>
                         </div>
                         {/* play button */}
                         <div className="element">
                             <div className="refresh-icon" onClick={() => isFinished && startPingTest()}>
                                 <div className={`switch demo1 ${(isReady) ? "active" : ""}`}>
-                                {/* <div className={`switch demo1 active`}> */}
-                                    <div className={`c-speedtest-lodig loding-5 loding-${pingCount * TOTAL_PINGS}`} id="speedProgress">
+                                    {/* <div className={`switch demo1 active`}> */}
+                                    <div className={`c-speedtest-lodig loding-${pingCount * 100 / TOTAL_PINGS}`} id="speedProgress">
                                         <svg viewBox="0 0 100 100"><path d="M 50,50 m 0,-47 a 47,47 0 1 1 0,94 a 47,47 0 1 1 0,-94" stroke="#eee" stroke-width="1" fill-opacity="0"></path><path d="M 50,50 m 0,-47 a 47,47 0 1 1 0,94 a 47,47 0 1 1 0,-94" stroke="#0DBE42" stroke-width="6" fill-opacity="0"></path>
                                         </svg>
                                     </div>
@@ -268,7 +283,7 @@ const PingComponent = () => {
                                                 <span
                                                     className="progress-bar"
                                                     style={{
-                                                        width: `${region.pings.length * TOTAL_PINGS}%`,
+                                                        width: `${region.pings.length * (100 / TOTAL_PINGS)}%`,
                                                     }}
                                                 />
                                             </div>
